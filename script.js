7a539bf7b64a8d5327d81c0f855849ec54ec3a5e
NurAsian <74744849+NurAsian@users.noreply.github.com>
2023-03-09 15:55:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function search(){
     event.preventDefault()
     var name = document.getElementById("keyword").value
     // window.open("https://www.youtube.com/results?search_query="+name, '_blank', 'location=yes,height=840,width=580,scrollbars=yes,status=yes')
-     window.open("https://www.youtube.com/@sompongpokasri5783/search?query="+name, '_blank', 'location=yes,height=840,width=580,scrollbars=yes,status=yes')
+     window.open("https://www.youtube.com/@sahahdalawiyah7600/search?query="+name, '_blank', 'location=yes,height=840,width=580,scrollbars=yes,status=yes')
     
 
 }
